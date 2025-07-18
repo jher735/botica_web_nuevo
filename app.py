2522b121205d7868e9979dfa41d20caf38c782cf
jher735 <jhermyyupanqui735@gmail.com>
2025-07-17 23:53:31 -0500

    Subo botica Flask

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
 import psycopg2
+from urllib.parse import urlparse
+import os
 from datetime import date
 
 app = Flask(__name__)
 
-# Conexión a la base de datos PostgreSQL
-conn = psycopg2.connect(
-    host="localhost",
-    database="Botica",
-    user="postgres",
-    password="1234567"  # cámbialo por tu contraseña real
-)
+# URL de Render - reemplaza esta por tu propia URL
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://botica_bd_user:pJ0E96luKaBjJ2QjhWU2MGCeYN8Cmzyh@dpg-d1ssm16r433s73emt7og-a.oregon-postgres.render.com/botica_bd')
+
+# Conexión a la base de datos
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,18 +22,15 @@ def registro():
         precio = float(request.form['precio'])
         fecha = request.form['fecha']
 
-        # INSERT con columnas explícitas (muy importante)
         cursor.execute("""
             INSERT INTO ventas (comprador, producto, cantidad, precio, fecha)
             VALUES (%s, %s, %s, %s, %s)
         """, (comprador, producto, cantidad, precio, fecha))
         conn.commit()
 
-    # Obtener las ventas para mostrar en la tabla
     cursor.execute("SELECT * FROM ventas")
     ventas = cursor.fetchall()
 
-    # Enviar HTML con datos y la fecha máxima
     return render_template('index.html', ventas=ventas, fecha_maxima=str(date.today()))
 
 if __name__ == '__main__':
